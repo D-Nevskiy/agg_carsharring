258dc48d2f3b3a1cf7642473b785d5bb0c179804
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-11-28 18:53:37 +0000

    added utils

---
+++ backend/core/texts.py
@@ -0,0 +1,19 @@
+# Текстовые строки
+HELP_TEXT_PHONE_NUMBER = "Номер телефона"
+HELP_TEXT_NAME = "Имя пользователя"
+HELP_TEXT_SURNAME = "Фамилия пользователя"
+HELP_TEXT_EMAIL = "Адрес электронной почты"
+HELP_TEXT_PASSPORT_IMAGE = "Изображение паспорта пользователя"
+HELP_TEXT_LICENSE_IMAGE = "Изображение водительского удостоверения пользователя"
+HELP_TEXT_SELFIE_IMAGE = "Селфи с документом пользователя"
+HELP_TEXT_VERIFICATION_STATUS = "Статус верификации пользователя"
+HELP_TEXT_USER_ACTIVITY = "Активность пользователя. Отключите, чтобы заблокировать пользователя."
+HELP_TEXT_ORDER_STATUS = "Состояние заказа."
+
+# Значения с выбором
+VERIFICATION_STATUS = [("unverified", "Неверифицирован"), ("verified", "Верифицирован")]
+ORDER_STATUS = [('Active', 'Активен'), ('Finished', 'Завершен')]
+
+# Цифровые значения
+MIN_LENGTH_VALIDATOR = 4
+DEFAULT_LENGHT = 255
