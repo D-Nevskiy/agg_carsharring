b1f021c4910475d0c71852797d41c8c99f9c9eb4
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-11-28 18:52:27 +0000

    added model Bonus

--- backend/bonuses/models.py
+++ backend/bonuses/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from users.models import User
+
+
+class Bonus(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="bonuses",
+    )
+    amount = models.IntegerField(default=0)
+    created_at = models.DateTimeField(
+        default=timezone.now,
+        editable=False,
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.email}'s Bonuses"
