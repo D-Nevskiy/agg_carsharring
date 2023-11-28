b986f8c3fe25e11cc6a76cb9ca2f872b054c5d2d
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-11-28 18:51:50 +0000

    added apps

--- backend/aggcarshering/settings.py
+++ backend/aggcarshering/settings.py
@@ -18,7 +18,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'api',
+    'phonenumber_field',
+    'users',
+    'bonuses',
+    'payment_cards',
+    'orders',
 ]
+
+AUTH_USER_MODEL = "users.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
