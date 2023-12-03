e26bc76b85ea78fbc7a52b6a956889cb1e1fb167
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-12-03 16:13:37 +0300

    Добавлены сериализатор и вьюхи для работы с данными об автомобилях.

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from cars.models import Car, EngineType, CarType, Company
 from users.models import User
 
 
@@ -17,3 +18,42 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
+
+class EngineTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EngineType
+        fields = ["id", "name"]
+
+
+class CarTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarType
+        fields = ["id", "name"]
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ["id", "name"]
+
+
+class CarSerializer(serializers.ModelSerializer):
+    engine_type = EngineTypeSerializer()
+    car_type = CarTypeSerializer()
+    company = CompanySerializer()
+
+    class Meta:
+        model = Car
+        fields = [
+            "id",
+            "is_available",
+            "company",
+            "name",
+            "model",
+            "engine_type",
+            "car_type",
+            "rating",
+            "coefficient",
+            "child_seat",
+        ]
