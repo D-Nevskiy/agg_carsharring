d1ac3d18992f14d882c46e9b4b593a9505863d09
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-12-03 16:14:43 +0300

    Добавлен URL-маршруты для вью CarListView.

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from .spectacular.urls import urlpatterns as doc_urls
 
-from .views import RegistrationAPIView, UsersAPIView
+from .views import CarListView, RegistrationAPIView, UsersAPIView
 
 
 app_name = "api"
@@ -10,6 +10,7 @@ app_name = "api"
 urlpatterns = [
     path("register/", RegistrationAPIView.as_view(), name="registration"),
     path("users/", UsersAPIView.as_view(), name="list_users"),
+    path("cars/", CarListView.as_view(), name="car-list"),
     path("", include("drfpasswordless.urls")),
 ]
 
