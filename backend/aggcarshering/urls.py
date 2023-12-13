0060845b216cd0e01085499f14088d7435c54527
Marat Shaibakov <smaspb17@yandex.ru>
2023-12-13 18:23:02 +0300

    Update swagger

--- backend/aggcarshering/urls.py
+++ backend/aggcarshering/urls.py
@@ -4,6 +4,7 @@ from drf_spectacular.views import SpectacularAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name='schema'),
     path("api/v1/", include("api.v1.urls")),
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+
 ]
