bf000a60216b789d858ad10cc43799a5c0a98a23
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-12-19 01:15:43 +0300

    added admin-panel for car model

--- backend/cars/admin.py
+++ backend/cars/admin.py
@@ -1,7 +1,62 @@
 from django.contrib import admin
-from cars.models import Car
+from django.utils.translation import gettext_lazy as _
+
+from .models import CoordinatesCar, Car
+
+
+@admin.register(CoordinatesCar)
+class CoordinatesCarAdmin(admin.ModelAdmin):
+    list_display = [
+        "latitude",
+        "longitude",
+    ]
+    search_fields = [
+        "latitude",
+        "longitude",
+    ]
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        "company",
+        "brand",
+        "model",
+        "is_available",
+        "type_car",
+    ]
+    search_fields = [
+        "company",
+        "brand",
+        "model",
+        "state_number",
+    ]
+
+    fieldsets = (
+        (
+            _("О машине"),
+            {
+                "fields": (
+                    "image",
+                    "is_available",
+                    "company",
+                    "brand",
+                    "model",
+                    "type_car",
+                    "state_number",
+                )
+            },
+        ),
+        (
+            _("Детали машин"),
+            {
+                "fields": (
+                    "type_engine",
+                    "child_seat",
+                    "power_reserve",
+                    "rating",
+                    "coordinates",
+                ),
+            },
+        ),
+    )
