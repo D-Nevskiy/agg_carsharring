5cf4d4a85d007e09a2670f854bafacad5ac1f2d9
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-12-19 01:17:03 +0300

    added CarViewSet

--- backend/cars/views.py
+++ backend/cars/views.py
@@ -1,9 +1,30 @@
+from django.db.transaction import atomic
+
+from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+
 from .models import Car
-from rest_framework.viewsets import ReadOnlyModelViewSet
 from .serializers import CarSerializer
 
 
-class CarViewSet(ReadOnlyModelViewSet):
+class CarViewSet(ModelViewSet):
+    """Представление для работы с публичными данными автомобилей."""
+
     queryset = Car.objects.all()
     serializer_class = CarSerializer
-    pagination_class = None
+    pagination_class = PageNumberPagination
+    permission_classes = [AllowAny]
+
+    @action(detail=False, methods=["post"])
+    @atomic
+    def create_car(self, request, *args, **kwargs):
+        """Создать новый автомобиль."""
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
