0ee4cbb6fe26477947b865b364e8370c2737250b
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-12-19 23:58:40 +0300

    removing extra username field

--- backend/users/models.py
+++ backend/users/models.py
@@ -26,7 +26,7 @@ class CustomUserManager(BaseUserManager):
             if not email:
                 raise ValueError("Поле email обязательно к заполнению.")
             email = self.normalize_email(email)
-            user = self.model(email=email, username=None, **extra_fields)
+            user = self.model(email=email, **extra_fields)
             user.set_password(password)
             user.save(using=self._db)
             return user
