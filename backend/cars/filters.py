d2b72a05b786d3e6d253850fddb503d15fd9c31f
Данил Воронин <melinbodb@yandex.ru>
2023-12-20 00:52:03 +0300

    coordinates filter

--- backend/cars/filters.py
+++ backend/cars/filters.py
@@ -6,7 +6,8 @@ class CarFilter(django_filters.FilterSet):
     type_car = django_filters.rest_framework.BaseInFilter()
     company = django_filters.rest_framework.BaseInFilter()
     type_engine = django_filters.rest_framework.BaseInFilter()
-    power_reserve = django_filters.rest_framework.BaseInFilter()
+    child_seat = django_filters.rest_framework.BooleanFilter()
+    is_available = django_filters.rest_framework.BooleanFilter()
     latitude = django_filters.rest_framework.RangeFilter(
             field_name='coordinates__latitude'
     )
