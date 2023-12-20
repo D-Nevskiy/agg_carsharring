8a744277ce781ef96379e90878b48c2d838e3e17
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-12-20 15:09:55 +0300

    Update settings.py

--- backend/aggcarshering/settings.py
+++ backend/aggcarshering/settings.py
@@ -141,6 +141,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
     "DEFAULT_PARSER_CLASSES": [
         "rest_framework.parsers.JSONParser",
         "rest_framework.parsers.FormParser",
