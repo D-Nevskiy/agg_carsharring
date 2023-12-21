feabeac34a61fe84d3a47379fd002143b0d3df7a
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-12-22 01:24:03 +0300

    added func send_confirmation_code, generate_reset_code

---
+++ backend/core/utils.py
@@ -0,0 +1,25 @@
+import random
+
+from django.conf import settings
+from django.core.mail import send_mail
+
+from core.texts import USER_RESET_CODE_LEN
+
+
+def send_confirmation_code(email, code):
+    """Функция отправки сообщений."""
+
+    subject = "Сброс пароля"
+    message = f"Ваш временный код для сброса пароля: {code}"
+    from_email = settings.DEFAULT_FROM_EMAIL
+    recipient_list = [email]
+    send_mail(subject, message, from_email, recipient_list)
+
+
+def generate_reset_code():
+    """Функция генерации кода."""
+
+    code = "".join(
+        [str(random.randint(0, 9)) for _ in range(USER_RESET_CODE_LEN)]
+    )
+    return code
